a30f044245d191d26e3678e6dd5def3088bd5e09
anphan2410 <an.phan@ascenx.com>
2017-06-26 08:39:10 +0700

    test qt linux

--- anlogger.h
+++ anlogger.h
@@ -5,7 +5,7 @@
 //_anLoggerEnabled Is Zero => Logger Is Globally Disabled
 #define _anLoggerEnabled 1
 //_anLoggerEnabled Is Zero => Logger Message Is Not Verbosely Positioned
-#define _anPositionEnabled 0
+#define _anPositionEnabled 1
 /************* Performance Flags ************************************************/
 #define _anMessagePathTextAttribute anDefaultTextAttribute
 #define _anThreadIdPositionEnabled 1
@@ -22,16 +22,16 @@
 #include <thread>
 #include <chrono>
 #include <time.h>
-#if defined _WINDOWS_ || defined __WINDOWS__ \
-        || defined _WIN16 || defined __TOS_WIN__ \
-        || defined _WIN32_WCE || defined _WIN32_WCE \
-        || defined WIN32 || defined _WIN32 || defined _WIN32_ \
-        || defined __WIN32 || defined __WIN32__ \
-        || defined _WIN32_WINNT || defined WINNT || defined _WINNT \
+#if defined _WINDOWS_ || defined __WINDOWS__\
+        || defined _WIN16 || defined __TOS_WIN__\
+        || defined _WIN32_WCE || defined _WIN32_WCE\
+        || defined WIN32 || defined _WIN32 || defined _WIN32_\
+        || defined __WIN32 || defined __WIN32__\
+        || defined _WIN32_WINNT || defined WINNT || defined _WINNT\
         || defined _WINNT_ || defined __WINNT || defined __WINNT__
     #include <windows.h>
     #define __anWINOS__
-#elif defined linux || defined _linux_ || defined __linux || defined __linux__ \
+#elif defined linux || defined _linux_ || defined __linux || defined __linux__\
         || defined __gnu_linux__
     #define __anLINUXOS__
 #endif
@@ -87,10 +87,10 @@ static const std::chrono::steady_clock::time_point anThisProgramStartingTimePoin
         }
         return false;
     }
-    #define _anGetConsoleTextAttribute(destination) \
+    #define _anGetConsoleTextAttribute(destination)\
         anGetCurrentConsoleTextAttribute(destination)
 
-    #define anSetConsoleTextAttribute(TextAtrribute) \
+    #define anSetConsoleTextAttribute(TextAtrribute)\
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), TextAtrribute);
 
     #define __anFilePathSlashChar__ u'\\'
@@ -109,9 +109,10 @@ static const std::chrono::steady_clock::time_point anThisProgramStartingTimePoin
 
     static bool anGetCurrentConsoleTextAttribute(anTxtAttribType &OutputVar) {
         std::string tmpBuff = std::string(anStdErrBuffer);
-        std::size_t tmp = tmpBuff.find_last_of(u8"\033[") + 1;
+        std::size_t tmp = tmpBuff.find_last_of(u8"\033[");
         if (tmp != std::string::npos)
         {
+            ++tmp;
             if ((tmpBuff.at(tmp) == u'0') && (tmpBuff.at(tmp+1) == u'm'))
                 OutputVar = 0;
             else if ((tmpBuff.at(tmp) == u'3') && (tmpBuff.at(tmp+2) == u'm'))
@@ -123,11 +124,11 @@ static const std::chrono::steady_clock::time_point anThisProgramStartingTimePoin
         else
             return false;
     }
-    #define _anGetConsoleTextAttribute(destination) \
+    #define _anGetConsoleTextAttribute(destination)\
         anGetCurrentConsoleTextAttribute(destination)
 
-    #define anSetConsoleTextAttribute(TxtAttrib) \
-        std::cerr << u8"\033[" \
+    #define anSetConsoleTextAttribute(TxtAttrib)\
+        std::cerr << u8"\033["\
         << std::to_string(TxtAttrib)\
         << u8"m";
 
@@ -142,8 +143,8 @@ static const anTxtAttribType anOriginalConsoleTextAttribute = [](){
 }();
 #define __anOriginalConsoleTextAttribute__ anOriginalConsoleTextAttribute
 
-#if _anPositionEnabled && (_anThreadIdPositionEnabled \
-        || _anFunctionPositionEnabled || _anFilePositionEnabled \
+#if _anPositionEnabled && (_anThreadIdPositionEnabled\
+        || _anFunctionPositionEnabled || _anFilePositionEnabled\
         || _anLinePositionEnabled || _anTimePositionEnabled)
 
     #if _anFilePositionEnabled
@@ -158,8 +159,8 @@ static const anTxtAttribType anOriginalConsoleTextAttribute = [](){
 
     #if _anTimePositionEnabled
 
-        #define __anElapsedTimeNSEC__ \
-            std::chrono::duration_cast<std::chrono::nanoseconds>( \
+        #define __anElapsedTimeNSEC__\
+            std::chrono::duration_cast<std::chrono::nanoseconds>(\
                 std::chrono::steady_clock::now() - __anStartTimePoint__).count()
     #endif
 
@@ -231,11 +232,11 @@ static const anTxtAttribType anOriginalConsoleTextAttribute = [](){
         #define anTmpLineParam
     #endif
 
-    #define __anMessagePath__ \
-        anCurrentMessagePath(anTmpTimeParam \
-                             anTmpThreadIdParam \
-                             anTmpFunctionParam \
-                             anTmpFileNameParam \
+    #define __anMessagePath__\
+        anCurrentMessagePath(anTmpTimeParam\
+                             anTmpThreadIdParam\
+                             anTmpFunctionParam\
+                             anTmpFileNameParam\
                              anTmpLineParam)
 
 #else
@@ -248,22 +249,22 @@ static const anTxtAttribType anOriginalConsoleTextAttribute = [](){
     #define anLogCode(...) __VA_ARGS__
 
 #ifdef __anQt__
-    #define anMsgInputToMsgString(msgIn,msgStr) \
-            std::string msgStr; \
-            { \
-                QString * tmpQStrrBefore##msgStr = new QString(u8""); \
-                QTextStream(tmpQStrrBefore##msgStr, QIODevice::ReadWrite) << msgIn; \
-                msgStr = tmpQStrrBefore##msgStr->toStdString(); \
-                delete tmpQStrrBefore##msgStr; \
-                tmpQStrrBefore##msgStr = Q_NULLPTR; \
+    #define anMsgInputToMsgString(msgIn,msgStr)\
+            std::string msgStr;\
+            {\
+                QString * tmpQStrrBefore##msgStr = new QString(u8"");\
+                QTextStream(tmpQStrrBefore##msgStr, QIODevice::ReadWrite) << msgIn;\
+                msgStr = tmpQStrrBefore##msgStr->toStdString();\
+                delete tmpQStrrBefore##msgStr;\
+                tmpQStrrBefore##msgStr = Q_NULLPTR;\
             }
 #else
-    #define anMsgInputToMsgString(msgIn,msgStr) \
-            std::string msgStr; \
-            { \
-                std::stringstream tmpStrStrmBefore##msgStr; \
-                tmpStrStrmBefore##msgStr << msgIn; \
-                msgStr = tmpStrStrmBefore##msgStr.str(); \
+    #define anMsgInputToMsgString(msgIn,msgStr)\
+            std::string msgStr;\
+            {\
+                std::stringstream tmpStrStrmBefore##msgStr;\
+                tmpStrStrmBefore##msgStr << msgIn;\
+                msgStr = tmpStrStrmBefore##msgStr.str();\
             }
 #endif
 
@@ -272,15 +273,15 @@ static const anTxtAttribType anOriginalConsoleTextAttribute = [](){
                                   const std::string &msgPath,
                                   const anTxtAttribType &preTxtAttrib) {
         std::cerr << aNoLineMessage;
-        anSetConsoleTextAttribute(_anMessagePathTextAttribute);
+        anSetConsoleTextAttribute(_anMessagePathTextAttribute)
         std::cerr << msgPath;
-        anSetConsoleTextAttribute(preTxtAttrib);
+        anSetConsoleTextAttribute(preTxtAttrib)
     }
     static void anColorizedMessageLogger(std::string &rawMsgStr,
                                          const anTxtAttribType &txtAttrib,
                                          const std::string &currentMsgPath,
                                          const anTxtAttribType &previousTxtAttrib) {
-        anSetConsoleTextAttribute(txtAttrib);
+        anSetConsoleTextAttribute(txtAttrib)
         std::string nowPath = u8"<-" + currentMsgPath;
         std::string tmpPath = nowPath + u8"\n";
         for(std::string::size_type i = 0;(i = rawMsgStr.find(u8"\n", 0)) != std::string::npos;)
@@ -293,28 +294,28 @@ static const anTxtAttribType anOriginalConsoleTextAttribute = [](){
         anSetConsoleTextAttribute(previousTxtAttrib)
     }
 
-    #define anMsg(msg, aTextAttribute) { \
-        std::string currentMessagePath = __anMessagePath__; \
-        anTxtAttribType prevTxtAttrib = 0; \
-        if (!(_anGetConsoleTextAttribute(prevTxtAttrib))) \
-            prevTxtAttrib = anOriginalConsoleTextAttribute; \
-        anMsgInputToMsgString(msg, tmpMsgStr) \
-        anColorizedMessageLogger(tmpMsgStr, aTextAttribute, currentMessagePath, prevTxtAttrib); \
+    #define anMsg(msg, aTextAttribute) {\
+        std::string currentMessagePath = __anMessagePath__;\
+        anTxtAttribType prevTxtAttrib = 0;\
+        if (!(_anGetConsoleTextAttribute(prevTxtAttrib)))\
+            prevTxtAttrib = anOriginalConsoleTextAttribute;\
+        anMsgInputToMsgString(msg, tmpMsgStr)\
+        anColorizedMessageLogger(tmpMsgStr, aTextAttribute, currentMessagePath, prevTxtAttrib);\
     }
 #else
 
-    #define anMsg(msg, txtAttrib) { \
-        anTxtAttribType previousTxtAttrib = 0; \
-        if (!(_anGetConsoleTextAttribute(previousTxtAttrib))) \
-            previousTxtAttrib = anOriginalConsoleTextAttribute; \
-        anSetConsoleTextAttribute(txtAttrib) \
-        anMsgInputToMsgString(msg, tmp) \
-        std::cerr << tmp; \
-        anSetConsoleTextAttribute(previousTxtAttrib) \
+    #define anMsg(msg, txtAttrib) {\
+        anTxtAttribType previousTxtAttrib = 0;\
+        if (!(_anGetConsoleTextAttribute(previousTxtAttrib)))\
+            previousTxtAttrib = anOriginalConsoleTextAttribute;\
+        anSetConsoleTextAttribute(txtAttrib)\
+        anMsgInputToMsgString(msg, tmp)\
+        std::cerr << tmp;\
+        anSetConsoleTextAttribute(previousTxtAttrib)\
     }
 #endif
 
-#define anDbg(msg, condition) if (condition) \
+#define anDbg(msg, condition) if (condition)\
                                 anMsg(u8"=> " << msg << "\n", anForegroundCyan)
 #define anInfo(msg) anMsg(u8"   " << msg << "\n", anForegroundWhite)
 #define anAck(msg) anMsg(u8"=> " << msg << "\n", anForegroundGreen)
--- main.cpp
+++ main.cpp
@@ -7,7 +7,14 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    anMsg("this is a test!", anForegroundGreen);
+    anSetConsoleTextAttribute(anForegroundCyan);
+
+    anInfo("This Type Is To Provide Info !");
+    anAck("This Type Is To Show Acknowledgement !");
+    anWarning("This Type Is To Show Warning !");
+    anError("This Type Is To Warn !");
+
+    std::cout << "END PROGRAM !" << std::endl;
 
     return a.exec();
 }
