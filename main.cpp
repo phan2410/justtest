bd22d7b7b0210b2c77881dfc7a1d10e4d2beabe1
anphan2410 <an.phan@ascenx.com>
2017-06-28 14:15:15 +0700

    test Thread Linux

--- main.cpp
+++ main.cpp
@@ -2,17 +2,13 @@
 #include <stdio.h>
 #include <iostream>
 #include "anlogger.h"
+#include <QThread>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
-    anInfo("This Type Is To Provide Info !");
-    anAck("This Type Is To Show Acknowledgement !");
-    anWarning("This Type Is To Show Warning !");
-    anError("This Type Is To Warn !");
-
-    std::cout << "END PROGRAM !" << std::endl;
+    QThread::currentThread()->setPriority(QThread::HighestPriority);
+    anAck(QThread::currentThread()->priority());
 
     return a.exec();
 }
